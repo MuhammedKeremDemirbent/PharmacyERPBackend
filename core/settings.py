08aa7e3bde443ae03dc908f729bb6b560dd5be1c
MuhammedKeremDemirbent <keremdmrbnt03@gmail.com>
2026-02-15 18:37:32 +0300

    Send Real Mail Accounts

--- core/settings.py
+++ core/settings.py
@@ -175,12 +175,21 @@ CELERY_BEAT_SCHEDULE = {
     },
 }
 
-# SMTP AYARLARI (MAILPIT)
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+"""
+Mailpit simülasyonu için
 EMAIL_HOST = 'mailpit'     # Docker servis adı
 EMAIL_PORT = 1025          # Mailpit SMTP portu
 EMAIL_USE_TLS = False      # Geliştirme ortamında şifreleme yok
 DEFAULT_FROM_EMAIL = 'admin@eczane.com'
+"""
+# SMTP AYARLARI 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com') #smtp.office365.com
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587)) #TLS ve SSL
+EMAIL_USE_TLS = True 
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')        
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD') # Gmail Uygulama Şifresi
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
